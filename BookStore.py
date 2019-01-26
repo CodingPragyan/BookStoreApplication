d09b14130118ea54f6ebc1d13298b9ea3247b6ac
CodingPragyan <47042372+CodingPragyan@users.noreply.github.com>
2019-01-26 21:14:02 +0530

    Update BookStore.py

--- BookStore.py
+++ BookStore.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+import datetime
 
 client = MongoClient()
 db = client["Book-Store"]
@@ -7,42 +8,77 @@ book_col = db.Books
 
 def add_book():
     book = {}
-    book["ISBN"] = input("ISBN:  ")
-    book["_id"] = book["ISBN"]
-    book["TITLE"] = input("TITLE:  ")
-    book["AUTHOR ID"] = input("AUTHOR ID:  ")
-    book["AUTHOR NAME"] = input("AUTHOR NAME:  ")
-    book["AUTHOR ADDRESS"] = input("AUTHOR ADDRESS:  ")
-    book["AUTHOR PHONE NUMBER"] = input("AUTHOR PHONE NUMBER:   ")
-    book["PUBLISHER ID"] = input("PUBLISHER ID:  ")
-    book["PUBLISHER NAME"] = input("PUBLISHER NAME:  ")
-    book["PUBLISHER ADDRESS"] = input("PUBLISHER ADDRESS:  ")
-    book["PUBLISHER PHONE NUMBER"] = input("PUBLISHER PHONE NUMBER:  ")
-    book["PRICE"] = int(input("PRICE OF THE BOOK:  "))
-    book["DATE"] = input("DATE OF PUBLISHING:  ")
-    book_col.insert_one(book)
-    print("BOOK INSERTED...")
+    isbn = input("ISBN:  ")
+    flag = check(isbn)
+    if flag < 1:
+        book["ISBN"] = isbn
+        book["_id"] = book["ISBN"]
+        book["TITLE"] = input("TITLE:  ")
+        book["AUTHOR ID"] = input("AUTHOR ID:  ")
+        book["AUTHOR NAME"] = input("AUTHOR NAME:  ")
+        book["AUTHOR ADDRESS"] = input("AUTHOR ADDRESS:  ")
+        author_phone = input("AUTHOR PHONE NUMBER:   ")
+        flag = check_is_digit(author_phone)
+        if flag == 0:
+            print("**WARNING!!**  The Author Phone number should be only number")
+            return
+        else:
+            book["AUTHOR PHONE NUMBER"] = author_phone
+        book["PUBLISHER ID"] = input("PUBLISHER ID:  ")
+        book["PUBLISHER NAME"] = input("PUBLISHER NAME:  ")
+        book["PUBLISHER ADDRESS"] = input("PUBLISHER ADDRESS:  ")
+        publisher_phone = input("PUBLISHER PHONE NUMBER:   ")
+        flag = check_is_digit(publisher_phone)
+        if flag == 0:
+            print("**WARNING!!**  The Publisher Phone number should be only number")
+            return
+        else:
+            book["PUBLISHER PHONE NUMBER"] = publisher_phone
+        price = input("PRICE OF THE BOOK:  ")
+        flag = check_is_digit(price)
+        if flag == 0:
+            print("**WARNING!!**  The Price should be only number")
+            return
+        else:
+            book["PRICE"] = price
+        date = input("DATE OF PUBLISHING (DD-MM-YY):  ")
+        flag = check_date(date)
+        if flag == 0:
+            print("**WARNING!!**  Not a valid format of date")
+            return
+        else:
+            book["DATE"] = date
+        book_col.insert_one(book)
+        print("BOOK INSERTED...")
+    else:
+        print(" ")
+        print("**WARNING!!**  Book with this ISBN already exists...")
+        print(" ")
 
-    
+
 def modify_book():
     isbn = input("Provide the ISBN of the book:  ")
     flag = check(isbn)
     if flag > 0:
         while True:
+            print(" ")
             print("MODIFY MENU")
             print("...............")
             print("1. Modify one attribute")
             print("2. Modify whole")
-            print("3. Go to Main Menu")
-            print("4. QUIT")
+            print("3. Delete a book")
+            print("4. Go to Main Menu")
+            print("5. QUIT")
             choice1 = input("Enter your choice:  ")
             if choice1 == "1":
                 modify_one(isbn)
             elif choice1 == "2":
                 modify_all(isbn)
             elif choice1 == "3":
-                break
+                delete_book(isbn)
             elif choice1 == "4":
+                break
+            elif choice1 == "5":
                 quit()
             else:
                 print("**OOPS!!**  Invalid input....")
@@ -56,9 +92,9 @@ def search_book():
     query = {"ISBN": x}
     book_document = book_col.find(query)
     print(" ")
-    print("DISPLAYING BOOK NUMBER "+x)
-    print(".............................")
     if flag > 0:
+        print("DISPLAYING BOOK NUMBER " + x)
+        print(".............................")
         for i in book_document:
             print(i)
     else:
@@ -143,6 +179,10 @@ def modify_one(num):
             print("AUTHOR ADDRESS updated...")
         elif choice2 == "5":
             author_phone = input("Enter the new AUTHOR PHONE NUMBER")
+            flag = check_is_digit(author_phone)
+            if flag == 0:
+                print("**WARNING!!**  The Author Phone number should be only number")
+                return
             book_col.update_one(
                 {"_id": num},
                 {
@@ -187,6 +227,10 @@ def modify_one(num):
             print("PUBLISHER ADDRESS updated...")
         elif choice2 == "9":
             publisher_phone = input("Enter the new PUBLISHER PHONE NUMBER")
+            flag = check_is_digit(publisher_phone)
+            if flag == 0:
+                print("**WARNING!!**  The Publisher Phone number should be only number")
+                return
             book_col.update_one(
                 {"_id": num},
                 {
@@ -198,6 +242,10 @@ def modify_one(num):
             print("PUBLISHER PHONE NUMBER updated...")
         elif choice2 == "10":
             price = input("Enter the new PRICE of the book")
+            flag = check_is_digit(price)
+            if flag == 0:
+                print("**WARNING!!**  The Price should be only number")
+                return
             book_col.update_one(
                 {"_id": num},
                 {
@@ -208,7 +256,11 @@ def modify_one(num):
             )
             print("PRICE updated...")
         elif choice2 == "11":
-            date = input("Enter the new publishing DATE")
+            date = input("Enter the new publishing DATE (DD-MM-YY)")
+            flag = check_date(date)
+            if flag == 0:
+                print("**WARNING!!**  Not a valid format of date")
+                return
             book_col.update_one(
                 {"_id": num},
                 {
@@ -226,18 +278,50 @@ def modify_one(num):
             print("**OOPS!!**   Invalid input.....")
 
 
+def check_is_digit(x):
+    try:
+        num = int(x)
+        return 1
+    except ValueError:
+        return 0
+
+
+def check_date(x):
+    try:
+        datetime.datetime.strptime(x, "%d-%m-%Y")
+        return 1
+    except ValueError:
+        return 0
+
+
 def modify_all(num):
     title = input("TITLE:  ")
     author_id = input("AUTHOR ID:  ")
     author_name = input("AUTHOR NAME:  ")
     author_address = input("AUTHOR ADDRESS:  ")
     author_phone = input("AUTHOR PHONE NUMBER:   ")
+    flag = check_is_digit(author_phone)
+    if flag == 0:
+        print("**WARNING!!**  The Author Phone number should be only number")
+        return
     publisher_id = input("PUBLISHER ID:  ")
     publisher_name = input("PUBLISHER NAME:  ")
     publisher_address = input("PUBLISHER ADDRESS:  ")
     publisher_phone = input("PUBLISHER PHONE NUMBER:  ")
-    price = int(input("PRICE OF THE BOOK:  "))
-    date = input("DATE OF PUBLISHING:  ")
+    flag = check_is_digit(publisher_phone)
+    if flag == 0:
+        print("**WARNING!!**  The Publisher Phone number should be only number")
+        return
+    price = input("PRICE OF THE BOOK:  ")
+    flag = check_is_digit(price)
+    if flag == 0:
+        print("**WARNING!!**  The Price should be only number")
+        return
+    date = input("DATE OF PUBLISHING(format: DD-MM-YY):  ")
+    flag = check_date(date)
+    if flag == 0:
+        print("**WARNING!!**  Not a valid format of date")
+        return
 
     book_col.update_many(
         {"_id": num},
@@ -259,96 +343,39 @@ def modify_all(num):
     )
 
 
+def delete_book(x):
+    my_query = {"_id": x}
+    book_col.delete_one(my_query)
+    print(" ")
+    print("Book deleted...")
+    print(" ")
+
+
 def check(x):
     query = {"ISBN": x}
     count = book_col.count_documents(query)
     return count
 
 
-def admin_login():
-    while True:
-        print("Welcome ADMIN. Choose your options")
-        print("..................................")
-        print("1  ADD A BOOK")
-        print("2  MODIFY A BOOK")
-        print("3  SEARCH A BOOK")
-        print("4  DISPLAY LIST OF BOOKS")
-        print("5  GO TO THE HOME PAGE")
-        print("6  QUIT")
-        choice3 = input("ENTER YOUR OPTION  ")
-        if choice3 == "1":
-            add_book()
-        elif choice3 == "2":
-            modify_book()
-            print("Book Store successfully updated.....")
-        elif choice3 == "3":
-            search_book()
-        elif choice3 == "4":
-            display()
-        elif choice3 == "5":
-            break
-        elif choice3 == "6":
-            print("Exited BookStore")
-            quit()
-        else:
-            print("**OOPS!!** Invalid Input....")
-
-
-def read_book():
-    print("Hope you enjoyed reading the book")
-
-
-def user_login():
-    while True:
-        print("Welcome USER. Choose your options")
-        print(".................................")
-        print("1.  READ eBOOK")
-        print("2.  SEARCH FOR A BOOK")
-        print("3.  DISPLAY ALL BOOKS")
-        print("4.  GO TO HOME PAGE")
-        print("5.  QUIT")
-        x = input("Enter your option here:  ")
-        if x == "1":
-            search_book()
-            z = input("Here is your soft copy of the book. Enjoy reading!! Press 1 if you are done:  ")
-            print(" ")
-            if z == "1":
-                pass
-            else:
-                print("**OOPS!!** Invalid Input....")
-        elif x == "2":
-            search_book()
-            y = input("Do you want to read it [Y/N]?")
-            if y.lower() == "y":
-                read_book()
-            elif y.lower() == "n":
-                break
-            else:
-                print("**OOPS!!** Invalid Input....")
-        elif x == "3":
-            display()
-        elif x == "4":
-            break
-        elif x == "5":
-            quit()
-        else:
-            print("**OOPS!!** Invalid Input....")
-
-
 while True:
-    print("Welcome to BOOK WORM, Read your favorite book online")
-    print("****************************************************")
-    print("Press 1 for USER LOGIN")
-    print("Press 2 for ADMIN LOGIN")
-    print("Press 3 to EXIT")
-    choice = input("Enter your choice here:  ")
-    if choice == "1":
-        print(">>>You have login as a user<<<")
-        user_login()
-    elif choice == "2":
-        print(">>>You have login as an admin<<<")
-        admin_login()
-    elif choice == "3":
+    print("WELCOME TO BOOK-WORM STORE")
+    print("..........................")
+    print("1  ADD A BOOK")
+    print("2  MODIFY A BOOK")
+    print("3  SEARCH A BOOK")
+    print("4  DISPLAY LIST OF BOOKS")
+    print("5  QUIT")
+    choice3 = input("ENTER YOUR OPTION  ")
+    if choice3 == "1":
+        add_book()
+    elif choice3 == "2":
+        modify_book()
+    elif choice3 == "3":
+        search_book()
+    elif choice3 == "4":
+        display()
+    elif choice3 == "5":
+        print("Exited BookStore")
         quit()
     else:
         print("**OOPS!!** Invalid Input....")
